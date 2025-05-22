96eb50abc15f0edc02036c99f5bb0574c0184df0
RobjeGamer <robbe.verberck@gmail.com>
2025-05-22 11:05:10 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
+let voeding = 20
+let knuffel = 20
+let dood = 0
+loops.everyInterval(1000, function () {
+    knuffel += -1
+    voeding += -1
+})
 basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
-        basic.showNumber(0)
-    } else {
-        basic.showNumber(1)
+    if (knuffel > 10 || voeding > 10) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        voeding += 5
+    }
+})
+basic.forever(function () {
+    if ((knuffel || voeding) > 0) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        knuffel += 5
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
     }
 })

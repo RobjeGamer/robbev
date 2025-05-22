99ee1a2fdde16c2b297e1d5114c51dd9d4d62120
RobjeGamer <robbe.verberck@gmail.com>
2025-05-22 11:31:01 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,32 +1,36 @@
-let voeding = 20
-let knuffel = 20
+input.onButtonPressed(Button.A, function () {
+    voeding += voeding + 5
+})
+input.onButtonPressed(Button.B, function () {
+    knuffel += knuffel + 5
+})
+let knuffel = 0
+let voeding = 0
+voeding = 20
+knuffel = 20
 let dood = 0
 loops.everyInterval(1000, function () {
-    knuffel += -1
-    voeding += -1
+    knuffel += knuffel - 1
+    voeding += voeding - 1
 })
 basic.forever(function () {
-    if (knuffel > 10 || voeding > 10) {
-        basic.showIcon(IconNames.Happy)
+    if (knuffel == 0 || voeding == 0) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
     }
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        voeding += 5
-    }
-})
-basic.forever(function () {
-    if ((knuffel || voeding) > 0) {
+    if (knuffel < 10 || voeding < 10) {
         basic.showIcon(IconNames.Sad)
     }
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        knuffel += 5
-    }
-})
-basic.forever(function () {
-    if (true) {
-    	
+    if (knuffel > 10 || voeding > 10) {
+        basic.showIcon(IconNames.Happy)
     }
 })
